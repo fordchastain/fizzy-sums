d96de868f6c3e6341a1197324b0a5e76432ba8fc
Ford Chastain <fordchastain@yahoo.com>
2021-04-02 23:40:12 -0500

    starting point

---
+++ src/components/SumPuzzle.js
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from '../logo.svg';
+import './SumPuzzle.css';
+
+export default class SumPuzzle extends React.Component {
+
+
+  render() {
+    return (
+      <div className="component-sum-puzzle" />
+    );
+  }
+  
+}
